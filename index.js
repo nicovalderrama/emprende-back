4c8fd0fe5f7d5bf2a9cdbbd5a2285d7b0af16afb
NicolasAValderrama <nicovalderrama2002@gmail.com>
2023-04-25 19:00:10 -0300

    cambio innecesario

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const http = require('http')
 
 function requestController(){
     //logica de la funcion
-
+    console.log('hola mundo')
 }
 
 //configurar servidor
 const server = http.createServer(requestController)
 
 port = process.env.PORT
-server.listen(port)
+server.listen(port, function(){
+    console.log(`aplicacion corriendo en el puerto :  ${port}`)
+})
