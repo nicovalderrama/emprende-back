6c0207b145bcaa00abc28aa84bd98056a514f8e1
NicolasAValderrama <nicovalderrama2002@gmail.com>
2023-04-28 15:32:59 -0300

    usando express

--- index.js
+++ index.js
@@ -1,48 +1,24 @@
 require('dotenv').config()
 const http = require('http')
 const fs = require('fs')
+const express = require('express')
+const app = express()
+const path = require('path')
 
-function requestController(req,res){
-    const url = req.url
-    const method = req.method
+//middlewares
+// app.use(express.static(path.join(__dirname + 'public')))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-    console.log({url,method})
-
-    if (method === 'GET' && url === '/') {
-        res.setHeader("Content-type", "text/html")
-        fs.readFile("./public/index.html", (err,file) => {
-            if (err) {
-                console.log('HUBO UN ERROR')
-            }
-            res.write(file)
-            res.end()
-        })
-        return
-    }
-
-    if (method === 'GET' && url === '/about') {
-        res.setHeader("Content-type", "text/html")
-        fs.readFile("./public/about.html", (err,file) => {
-            if (err) {
-                console.log("HUBO UN ERROR")
-            }
-            res.write(file)
-            res.end()
-        })
-        return
-    }
-
-    res.setHeader("Content-type","text/html; charset=utf-8")
-    res.write("<h1>Pagina no encontrada</h1>")
-    res.end()
-
-}
-
-//configurar servidor
-const server = http.createServer(requestController)
+app.get('/',(req,res) => {
+    res.render('index')
+})
+app.get('/about',(req,res) => {
+    res.render('about')
+})
 
 const port = process.env.PORT
 
-server.listen(port, function(){
-    console.log(`aplicacion corriendo en el puerto :  ${port}`)
+app.listen(port,() => {
+    console.log(`escuchando en el puerto ${port}`)
 })
